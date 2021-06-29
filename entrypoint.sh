e3784e34300ae68a3d44f17b89857ef3ae2eae61
sya-ri <sya79lua@gmail.com>
2021-06-30 01:48:02 +0900

    add generate settings.toml

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,11 @@ STRICT_ZIP_SPEC_COMPLIANCE=${3}
 COMPRESS_ALREADY_COMPRESSED_FILES=${4}
 IGNORE_SYSTEM_AND_HIDDEN_FILES=${5}
 ALLOW_MOD_OPTIFINE=${6}
+ALLOW_MODS="["
+if [ $ALLOW_MOD_OPTIFINE = "true" ]; then
+    ALLOW_MODS=$ALLOW_MODS"\"OptiFine\""
+fi
+ALLOW_MODS=$ALLOW_MODS"]"
 SAMPLING_FREQUENCY=${7}
 TARGET_PITCH=${8}
 MINIMUM_BITRATE=${9}
@@ -17,15 +22,23 @@ OUTPUT=${12}
 # print version
 echo PackSquash version: "$(packsquash --version)"
 
-echo $PATH
-echo $SKIP_PACK_ICON
-echo $STRICT_ZIP_SPEC_COMPLIANCE
-echo $COMPRESS_ALREADY_COMPRESSED_FILES
-echo $IGNORE_SYSTEM_AND_HIDDEN_FILES
-echo $ALLOW_MOD_OPTIFINE
-echo $SAMPLING_FREQUENCY
-echo $TARGET_PITCH
-echo $MINIMUM_BITRATE
-echo $MAXIMUM_BITRATE
-echo $QUANTIZE_IMAGE
-echo $OUTPUT
+# generate settings
+echo $'
+resource_pack_directory = '$PATH'
+skip_pack_icon = '$SKIP_PACK_ICON'
+strict_zip_spec_compliance = '$STRICT_ZIP_SPEC_COMPLIANCE'
+compress_already_compressed_files = '$COMPRESS_ALREADY_COMPRESSED_FILES'
+ignore_system_and_hidden_files = '$IGNORE_SYSTEM_AND_HIDDEN_FILES'
+allowed_mods = '$ALLOW_MODS'
+output_file_path = '$OUTPUT'
+
+["assets/*/sounds/{music,ambience}/?*.{og[ga],mp3,wav,flac}"]
+sampling_frequency = '$SAMPLING_FREQUENCY'
+minimum_bitrate = '$MINIMUM_BITRATE'
+maximum_bitrate = '$MAXIMUM_BITRATE'
+
+["**/*.png"]
+quantize_image = '$QUANTIZE_IMAGE'
+' > packsquash-settings.toml
+
+cat packsquash-settings.toml
